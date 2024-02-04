a1961d1f4839d524d64a2bae5cdd5080d792be77
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-02-05 01:11:46 +0500

    fixed trending and toprated title

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -38,10 +38,10 @@ const Dashboard = () => {
         {popularToggle === 'movie' ? <PopularMovies /> : <PopularTVShows />}
       </div>
 
-      <div className="mb-8">
+      <div className="mb-8 sm:text-start text-center">
       <h1 className="movies-wrapper-title scale-75 sm:scale-100 inline-block text-2xl 
         md:text-3xl lg:text-4xl font-bold hover:-translate-y-1 w-fit transition duration-300
-         ease-in-out m-1 p-2 sm:m-2 sm:p-4 md:ml-32">Trending</h1>
+         ease-in-out m-1 p-2 sm:m-2 sm:p-4 sm:ml-24">Trending</h1>
         <div className="inline-block mb-4 sm:scale-100 scale-75">
           <button
             className={`mr-2 px-4 py-2 font-bold rounded-md text-white  ${trendingToggle === 'movie' ? 'bg-cyan-600' : 'bg-gray-400'}`}
@@ -59,10 +59,10 @@ const Dashboard = () => {
         {trendingToggle === 'movie' ? <TrendingMovies /> : <TrendingTVShows />}
       </div>
 
-      <div className="mb-8">
+      <div className="mb-8 sm:text-start text-center">
       <h1 className="movies-wrapper-title inline-block text-2xl md:text-3xl lg:text-4xl 
       font-bold hover:-translate-y-1 w-fit transition 
-       p-2 sm:m-2 sm:p-4 md:ml-32 scale-75 sm:scale-100
+       p-2 sm:m-2 sm:p-4 sm:ml-24 scale-75 sm:scale-100
       duration-300 ease-in-out ">Top Rated</h1>
         <div className="inline-block mb-4 scale-75 sm:scale-100">
           <button
